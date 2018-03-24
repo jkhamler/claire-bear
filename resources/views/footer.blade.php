cdda12ef56cab8712bc351dfdbc2d0b50190ae75
jonathanhamler <jkhamler@gmail.com>
2018-03-24 14:32:44 +0000

    Improvements

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,36 @@
-<footer class="footer">
-    <div class="container">
-        <span class="text-muted">Place footer content here.</span>
+<footer class="page-footer footer-style">
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-10 footer-text">
+                <p>&#169; Claire Horne 2018 - All Rights Reserved</p>
+            </div>
+
+            <div class="col-2">
+
+                <table  style="margin-top: 8px;">
+                    <tr>
+                        <td style="padding: 10px;">
+                            <a href="https://www.pinterest.co.uk/clairehorne/"><i class="fab fa-pinterest fa-lg footer-icons"></i></a>
+                        </td>
+                        <td style="padding: 10px;">
+                            <a href="http://www.instagram.com/clairehorne"><i class="fab fa-instagram fa-lg footer-icons"></i></a>
+                        </td>
+                        <td style="padding: 10px;">
+                            <a href="http://www.linkedin.com/in/clairelhorne"><i class="fab fa-linkedin-in fa-lg footer-icons"></i></a>
+                        </td>
+                        <td style="padding: 10px;">
+                            <a href="http://www.behance.net/clairehorne"><i class="fab fa-behance fa-lg footer-icons"></i></a>
+                        </td>
+                    </tr>
+                </table>
+
+
+            </div>
+
+
+        </div>
+
     </div>
 </footer>
