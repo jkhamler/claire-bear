d1907f54b742b45a573e68b86d4b8f9980b06a20
jonathanhamler <jkhamler@gmail.com>
2018-04-12 15:39:30 +0200

    Improvements

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -8,18 +8,19 @@
 
         <div class="row" style="padding: 10px;">
 
+
             @foreach($projects as $project)
 
                 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3" style="padding: 0px;">
 
-                    <div class="content">
+                    <div class="content" style="margin: 5px;">
 
                         <a href="{{ $project['url'] }}" target="_blank">
 
-                            <div class="content-overlay"></div>
+                            <div class="content-overlay" style="padding: 100px;"></div>
 
                             <img class="content-image img-fluid"
-                                 src="{{ $project['thumbnail_path'] }}" style="padding: 10px;">
+                                 src="{{ $project['thumbnail_path'] }}">
 
                             <div class="content-details fadeIn-bottom">
                                 <h4 class="content-title">{{ $project['name'] }}</h4>
